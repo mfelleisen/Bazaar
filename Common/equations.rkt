ec5d55a0ff441367dfe5909e3c1dfbf78524e230
Matthias Felleisen <matthias@ccs.neu.edu>
2024-07-11 10:37:29 -0400

    bring up to standards

--- Common/equations.rkt
+++ Common/equations.rkt
@@ -1,36 +1,77 @@
 #lang racket
 
-(provide
- #; {type [Equation* X] = [Listof [Equation X]]}
- #; {type [Equation X]}
-
- render
-
- 1eq)
+;; a data representation of (parameteric) equations
 
 ;; ---------------------------------------------------------------------------------------------------
-(require "bags.rkt")
+(provide
+ #; {type [Equation* X] = [Listof [1Equation X]]}
+ #; {type [1Equation X]}
+
+
+ #; {[Listof [1Equation X]] [Bag X] [Bag X] -> [Listof [1Equation X]]}
+ #; (useful equation* my-wallet bank) 
+ ;; return those equations `e` in `equation*` for which `my-wallet` has enough Xs to swap
+ ;; one side and `bank`has enough Xs for the other; orient the resulting equations
+ ;; so that the left's side is below `my-wallet` 
+ useful
+ 
+ #; {[1Equation X] [X -> Pict] -> Pict}
+ #; (render e render-x)
+ ;; returns an image of `e` where `render-x` is used to render individual Xs
+ render
+
+ #; {[Bag X] [Bag X] -> 1Equation}
+ 1eq)
+
+(module+ examples
+  (provide EQ1 EQ1-rev)
+  (provide EQ1* EQ1-rev*))
+
+;; ---------------------------------------------------------------------------------------------------
+;; dependencies 
+
+(require Bazaar/Common/bags)
 (require pict)
 
 (module+ test (require rackunit))
+(module+ test (require (submod ".." examples)))
 
 ;; ---------------------------------------------------------------------------------------------------
+;; data definitions and examples 
+
 (struct 1eq [left right] #:prefab)
 #; {type Equation = (1eq Side Side)}
 #; {type Side     = b:Bag || (<= 1 (bag-size b) 4)}
 
-#; {Equation* Bag -> [Listof Equation]}
-;; return all those equations `e` for which `(subbag? (1eq-left e) bag)` `(subbag? (1eq-right e) bag)`
-;;   ordered so that the left satisfies the condition; meaning the same equation can show up twice  
-(define (useful equation* bag)
+(module+ examples 
+  #; 1Equation 
+  (define EQ1 [1eq '[1 1 2] '[3]])
+  (define EQ1-rev [1eq '[3] '[1 1 2]])
+
+
+  #; [Listof 1Equatin]
+  (define EQ1* [list EQ1])
+  (define EQ1-rev*  [list EQ1-rev]))
+
+;; ---------------------------------------------------------------------------------------------------
+;; functionality 
+
+(define (useful equation* my-wallet bank)
   (define sym (map 1eq-flip equation*))
-  (for/fold ([result '()]) ([e (append equation* sym)] #:when (subbag? (1eq-left e) bag))
+  (for/fold ([result '()]) ([e (append equation* sym)] #:when (can-swap e my-wallet bank))
     (cons e result)))
-    
+
+#; {1Equation Bag Bad -> Boolean}
+;; can `my-wallet` swap with `bank` according to `e`? 
+(define (can-swap e my-wallet bank)
+  (and (subbag? (1eq-left e) my-wallet) (subbag? (1eq-right e) bank)))
+
+#; {1Equation -> 1Equation}
 (define (1eq-flip e)
   (1eq (1eq-right e) (1eq-left e)))
 
-(define EQ1 [1eq '[1 1 2] '[3]])
+;; ---------------------------------------------------------------------------------------------------
+;; graphical representation 
 
 (define (render 1eq render-element)
   (define left (apply hc-append 2 (map render-element (1eq-left 1eq))))
@@ -39,9 +80,13 @@
 
 ;; ---------------------------------------------------------------------------------------------------
 (module+ test
-  (define equations [list EQ1])
-  (define reversed  [list [1eq '[3] '[1 1 2]]])
-
   (render EQ1 (λ (x) (text (~a x))))
 
-  (check-equal? (useful [list [1eq '[1 1 2] '[3]]] '[1 1 2 3]) (append reversed equations)))
+  (define my-wallet '[1 1 2 3])
+  (define bank      '[1 1 2 3])
+  (define low-bank  '[3])
+
+  (check-equal? (useful [list [1eq '[1 1 2] '[3]]] my-wallet bank) (append EQ1-rev* EQ1*))
+  
+  (check-true (can-swap EQ1 my-wallet low-bank))
+  (check-equal? (useful [list [1eq '[1 1 2] '[3]]] my-wallet low-bank) EQ1*))
