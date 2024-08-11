c6c6fd0f7f025dba7fada831076be40ea6da1a23
Matthias Felleisen <matthias@ccs.neu.edu>
2024-08-11 14:42:59 +0200

    json

--- Common/actions.rkt
+++ Common/actions.rkt
@@ -16,11 +16,49 @@
  #; (list c1 ...) ;; is a request to purchase the specified cards in order 
  buy-cards?)
 
+(module+ json
+  (provide
+   action->jsexpr
+   jsexpr->action))
+
 ;; -----------------------------------------------------------------------------
 (require (prefix-in c: Bazaar/Common/cards))
 (require (prefix-in e: Bazaar/Common/equations))
+
+(module+ json
+  (require (submod Bazaar/Common/cards json))
+  (require (submod Bazaar/Common/equations json))
+  (require Bazaar/Lib/parse-json)
+  (require SwDev/Lib/should-be-racket))
+
+(module+ test
+  (require (submod ".." json))
+  (require (submod Bazaar/Common/cards examples))
+  (require (submod Bazaar/Common/equations examples))
+  (require rackunit))
 
 ;; -----------------------------------------------------------------------------
 (define want-pebble? false?)
 (define trades?      (listof e:1eq?))
 (define buy-cards?   (listof c:card?))
+
+;; -----------------------------------------------------------------------------
+(module+ json
+  (define (action->jsexpr a)
+    (cond
+      [(want-pebble? a) a]
+      [(trades? a)      (equations->jsexpr a)]
+      [(buy-cards? a)   (card*->jsexpr a)]))
+
+  (define (jsexpr->action j)
+    (cond
+      [(false? j) #false]
+      [else (or (dev/null (jsexpr->trades j)) (dev/null (jsexpr->cards j)))]))
+
+  (def/jsexpr-> cards  #:array [(list (app jsexpr->card (? c:card? c)) ...) c])
+  (def/jsexpr-> trades #:array [(list (app jsexpr->1eq (? e:1eq? e)) ...) e]))
+
+(module+ test
+  (check-equal? (jsexpr->action (action->jsexpr #false)) #false)
+  (check-equal? (jsexpr->action (action->jsexpr `[,c-ggggg])) `[,c-ggggg])
+  (check-equal? (jsexpr->action (action->jsexpr `[,3xg=r-])) `[,3xg=r-]))
