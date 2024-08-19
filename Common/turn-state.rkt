1b2ffc4e77abaddbd3fb2b7cb26724098f2f1e27
Matthias Felleisen <matthias@ccs.neu.edu>
2024-08-19 10:50:34 -0400

    equip turn with generic equality

--- Common/turn-state.rkt
+++ Common/turn-state.rkt
@@ -86,10 +86,27 @@
 
 (struct/description
  turn
- [bank   #:to-jsexpr bag->jsexpr #:from-jsexpr jsexpr->bag #:is-a "*Pebbles"]
+ [bank   #:to-jsexpr bag->jsexpr    #:from-jsexpr jsexpr->bag #:is-a "*Pebbles"]
  [cards  #:to-jsexpr card*->jsexpr  #:from-jsexpr jsexpr->card* #:is-a "*Cards"]
  [active #:to-jsexpr player->jsexpr #:from-jsexpr jsexpr->player #:is-a "Player"]
- [scores #:to-jsexpr score*->jsexpr #:from-jsexpr jsexpr->score* #:is-a "*Naturals"])
+ [scores #:to-jsexpr score*->jsexpr #:from-jsexpr jsexpr->score* #:is-a "*Naturals"]
+ #:transparent
+ #:methods gen:equal+hash
+ [(define equal-proc
+    (λ (x y recursive-equal?)
+      (match-define [turn x-bank x-cards x-active x-scores] x)
+      (match-define [turn y-bank y-cards y-active y-scores] y)
+      (and (equal? x-scores y-scores)
+           (b:bag-equal? x-bank y-bank)
+           (equal? x-active y-active)
+           (= (length x-cards) (length y-cards))
+           (b:bag-equal? (apply b:bag x-cards) (apply b:bag y-cards)))))
+  (define (hash-proc x re-hash)
+    (+ (* 1000 (re-hash (turn-bank x)))
+       (* 10 (re-hash (turn-bank x)))))
+  (define (hash2-proc x re-hash)
+    (+ (* 1000 (re-hash (turn-bank x)))
+       (* 10 (re-hash (turn-bank x)))))])
 
 (define turn-wallet (compose p:player-wallet turn-active))
 
@@ -99,7 +116,17 @@
   (define ts-20        (turn b-r     [list c-ggggg]         p-rrbrr-20 '(6)))
   (define ts-20-rotate (turn b-r     [list c-ggggg]         p-r6       '(20)))
   (define ts-6-players (turn b-r     (list c-ggggg)         p-4xb-3xg4 '(5 6 7 8 9)))
-  (define ts-3-zeros   (turn b-rrbrr (list c-wyrbb c-ggggg) p-ggggg     '[0 0])))
+  (define ts-3-zeros   (turn b-rrbrr (list c-wyrbb c-ggggg) p-ggggg     '[0 0]))
+
+  ;; for equality testing
+  (provide ts1-card-swap ts1-wallet)
+  (define ts1-card-swap (turn b-ggggg [list c-rrbrr c-ggggg c-rgbrg] p-r6 '[]))
+  (define ts1-wallet    (turn (b:bag-add b-g b-gggg) [list c-rrbrr c-ggggg c-rgbrg] p-r6 '[])))
+
+(module+ test
+  (check-equal? ts1 ts1-wallet "construct wallet")
+  (check-equal? ts1 ts1-card-swap "cards swapped")
+  (check-false (equal? ts0 ts1)))
                                    
 
 
