99424189be1fb37a6dffeaec1e15343e610c6080
Matthias Felleisen <matthias@ccs.neu.edu>
2024-10-07 19:42:01 -0400

    card sequence comparison

--- Common/cards.rkt
+++ Common/cards.rkt
@@ -188,7 +188,8 @@
 
 #; {[NEListof Card] [NEListof Card] -> Boolean}
 (define (card*<? 1loc 2loc)
-  (for/and ([c 1loc] [d 2loc]) (card<? c d)))
+  (or (< (length 1loc) (length 2loc))
+      (for/and ([c 1loc] [d 2loc]) (card<? c d))))
 
 #; {Card Card -> Boolean}
 ;; one card is smaller than another if either
